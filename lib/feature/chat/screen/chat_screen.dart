b300fddf318474dfa001488c7e2007ceff5d23d3
sangeethck <sangeeth@tigabits.com>
2025-08-02 18:42:53 +0530

    BUG FIXES

--- lib/feature/chat/screen/chat_screen.dart
+++ lib/feature/chat/screen/chat_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:soxo_chat/feature/chat/screen/widgets/build_chat_item.dart';
+import 'package:soxo_chat/feature/chat/screen/widgets/build_tab.dart';
 import 'package:soxo_chat/shared/constants/colors.dart';
 import 'package:soxo_chat/shared/themes/font_palette.dart';
 import 'package:soxo_chat/shared/widgets/padding/main_padding.dart';
@@ -79,13 +81,13 @@ class ChatScreen extends StatelessWidget {
                         children: [
                           Row(
                             children: [
-                              _buildTab('All', true, width: 20.w),
+                              buildTab('All', true, width: 20.w),
                               6.horizontalSpace,
-                              _buildTab('Group Chat', false, width: 10.w),
+                              buildTab('Group Chat', false, width: 10.w),
                               6.horizontalSpace,
-                              _buildTab('Personal Chat', false, width: 10.w),
+                              buildTab('Personal Chat', false, width: 10.w),
                               6.horizontalSpace,
-                              _buildTab('Broadcast', false, width: 10.w),
+                              buildTab('Broadcast', false, width: 10.w),
                             ],
                           ),
                         ],
@@ -99,7 +101,7 @@ class ChatScreen extends StatelessWidget {
                           vertical: 0,
                         ),
                         children: [
-                          _buildChatItem(
+                          buildChatItem(
                             'RD',
                             Colors.blue,
                             'Internal Review',
@@ -107,7 +109,7 @@ class ChatScreen extends StatelessWidget {
                             'Today',
                             1,
                           ),
-                          _buildChatItem(
+                          buildChatItem(
                             'AT',
                             Colors.green,
                             'Anoop Ts',
@@ -115,7 +117,7 @@ class ChatScreen extends StatelessWidget {
                             '17/5',
                             0,
                           ),
-                          _buildChatItem(
+                          buildChatItem(
                             'CS',
                             Colors.deepOrange,
                             'Case Study\'s',
@@ -123,7 +125,7 @@ class ChatScreen extends StatelessWidget {
                             'Today',
                             1,
                           ),
-                          _buildChatItem(
+                          buildChatItem(
                             'RD',
                             Colors.blue,
                             'Internal Review',
@@ -149,106 +151,6 @@ class ChatScreen extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: SvgPicture.asset('assets/icons/Group 1000007039.svg'),
         ),
-      ),
-    );
-  }
-
-  Widget _buildTab(String text, bool isSelected, {double? width}) {
-    return Container(
-      height: 32.h,
-      padding: EdgeInsets.symmetric(horizontal: width ?? 14, vertical: 6),
-      decoration: BoxDecoration(
-        color: isSelected ? Colors.white : Colors.transparent,
-        borderRadius: BorderRadius.circular(28.r),
-        border: Border.all(
-          color: isSelected ? kPrimaryColor : Color(0XFFE3E3E3),
-        ),
-      ),
-      child: Text(
-        text,
-        style: FontPalette.hW500S12.copyWith(color: Color(0XFF0F1828)),
-      ),
-    );
-  }
-
-  Widget _buildChatItem(
-    String initials,
-    Color avatarColor,
-    String name,
-    String message,
-    String time,
-    int unreadCount,
-  ) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 12),
-      padding: EdgeInsets.only(left: 6.w, right: 8.w, bottom: 6.h, top: 6.h),
-      decoration: BoxDecoration(
-        color: kWhite,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Row(
-        children: [
-          Container(
-            width: 48.w,
-            height: 48.h,
-            decoration: BoxDecoration(
-              color: avatarColor,
-              borderRadius: BorderRadius.all(Radius.circular(16.r)),
-            ),
-            child: Center(
-              child: Text(
-                initials,
-                style: FontPalette.hW700S14.copyWith(color: kWhite),
-              ),
-            ),
-          ),
-          SizedBox(width: 12),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(name, style: FontPalette.hW600S14),
-                SizedBox(height: 4),
-                Text(
-                  message,
-                  style: FontPalette.hW400S12.copyWith(
-                    color: Color(0XFFADB5BD),
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                time,
-                style: TextStyle(color: Color(0XFFA4A4A4), fontSize: 12),
-              ),
-              if (unreadCount > 0) ...[
-                10.verticalSpace,
-                Container(
-                  alignment: Alignment.center,
-                  height: 20.h,
-                  width: 20.w,
-                  padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-                  decoration: BoxDecoration(
-                    color: Color(0XFFD2D5F9),
-                    shape: BoxShape.circle,
-                  ),
-                  child: Text(
-                    unreadCount.toString(),
-                    style: FontPalette.hW500S10.copyWith(
-                      color: Color(0xFF001569),
-                    ),
-                  ),
-                ),
-              ],
-            ],
-          ),
-        ],
       ),
     );
   }
---
+++ lib/feature/chat/screen/widgets/build_chat_item.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:soxo_chat/shared/constants/colors.dart';
+import 'package:soxo_chat/shared/themes/font_palette.dart';
+
+Widget buildChatItem(
+  String initials,
+  Color avatarColor,
+  String name,
+  String message,
+  String time,
+  int unreadCount,
+) {
+  return Container(
+    margin: EdgeInsets.only(bottom: 12),
+    padding: EdgeInsets.only(left: 6.w, right: 8.w, bottom: 6.h, top: 6.h),
+    decoration: BoxDecoration(
+      color: kWhite,
+      borderRadius: BorderRadius.circular(12),
+    ),
+    child: Row(
+      children: [
+        Container(
+          width: 48.w,
+          height: 48.h,
+          decoration: BoxDecoration(
+            color: avatarColor,
+            borderRadius: BorderRadius.all(Radius.circular(16.r)),
+          ),
+          child: Center(
+            child: Text(
+              initials,
+              style: FontPalette.hW700S14.copyWith(color: kWhite),
+            ),
+          ),
+        ),
+        SizedBox(width: 12),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(name, style: FontPalette.hW600S14),
+              SizedBox(height: 4),
+              Text(
+                message,
+                style: FontPalette.hW400S12.copyWith(color: Color(0XFFADB5BD)),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              time,
+              style: TextStyle(color: Color(0XFFA4A4A4), fontSize: 12),
+            ),
+            if (unreadCount > 0) ...[
+              10.verticalSpace,
+              Container(
+                alignment: Alignment.center,
+                height: 20.h,
+                width: 20.w,
+                padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                decoration: BoxDecoration(
+                  color: Color(0XFFD2D5F9),
+                  shape: BoxShape.circle,
+                ),
+                child: Text(
+                  unreadCount.toString(),
+                  style: FontPalette.hW500S10.copyWith(
+                    color: Color(0xFF001569),
+                  ),
+                ),
+              ),
+            ],
+          ],
+        ),
+      ],
+    ),
+  );
+}
