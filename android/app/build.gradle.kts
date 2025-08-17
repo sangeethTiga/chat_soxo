610b79f854e0664034871db9aa284ee5c7bede39
sangeethck <sangeeth@tigabits.com>
2025-08-17 19:39:59 +0530

    BUG FIXES

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.soxo_chat"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdkVersion flutter.minSdkVersion
+        minSdk = flutter.minSdkVersion
         targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
