2581242fbaca19c620813780779255c3066acfa6
sangeethck <sangeeth@tigabits.com>
2025-08-23 15:12:08 +0530

    BUG FIXES

--- lib/feature/chat/screen/widgets/chat_bubble_widget.dart
+++ lib/feature/chat/screen/widgets/chat_bubble_widget.dart
@@ -77,6 +77,7 @@ class _ChatBubbleMessageState extends State<ChatBubbleMessage>
   final bool _isReplyFromMe = false;
   final Set<int> _pinnedMessageIds = {};
   late Map<String, GlobalKey> _messageKeys;
+
   @override
   void initState() {
     super.initState();
@@ -117,9 +118,10 @@ class _ChatBubbleMessageState extends State<ChatBubbleMessage>
           ? Colors.green.withOpacity(0.15)
           : Colors.blue.withOpacity(0.15);
     } else {
+      // Updated colors for uniform alignment - distinguish by color only
       _bubbleColor = widget.isSent
-          ? const Color(0xFFE6F2EC)
-          : Colors.grey[200]!;
+          ? const Color(0xFFE6F2EC) // Light green for sent messages
+          : Colors.grey[200]!; // Light blue for received messages
       _replyBorderColor = widget.isSent ? Colors.green : Colors.blue;
       _replyBackgroundColor = widget.isSent
           ? Colors.green.withOpacity(0.1)
@@ -166,7 +168,8 @@ class _ChatBubbleMessageState extends State<ChatBubbleMessage>
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
     if (!_isDragging) return;
 
-    double delta = widget.isSent ? -details.delta.dx : details.delta.dx;
+    // For uniform alignment, always use right-to-left swipe for reply
+    double delta = -details.delta.dx;
 
     if (delta > 0) {
       setState(() {
@@ -216,7 +219,6 @@ class _ChatBubbleMessageState extends State<ChatBubbleMessage>
           children: [
             if (_dragDistance > 0)
               _ReplyIcon(
-                isSent: widget.isSent,
                 hasTriggeredReply: _hasTriggeredReply,
                 animation: _replyIconAnimation,
               ),
@@ -226,10 +228,7 @@ class _ChatBubbleMessageState extends State<ChatBubbleMessage>
                   : const Duration(milliseconds: 300),
               curve: Curves.easeOut,
               transform: Matrix4.identity()
-                ..translate(
-                  widget.isSent ? -_dragDistance : _dragDistance,
-                  0.0,
-                ),
+                ..translate(-_dragDistance, 0.0), // Always swipe left for reply
               child: _MessageContainer(
                 widget: widget,
                 bubbleColor: _bubbleColor,
@@ -381,22 +380,17 @@ class _ChatBubbleMessageState extends State<ChatBubbleMessage>
   }
 }
 
+// Updated Reply Icon - removed isSent parameter since it's now uniform
 class _ReplyIcon extends StatelessWidget {
-  final bool isSent;
   final bool hasTriggeredReply;
   final Animation<double> animation;
 
-  const _ReplyIcon({
-    required this.isSent,
-    required this.hasTriggeredReply,
-    required this.animation,
-  });
+  const _ReplyIcon({required this.hasTriggeredReply, required this.animation});
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      right: isSent ? 20.w : null,
-      left: isSent ? null : 20.w,
+      right: 20.w, // Always positioned on the right for uniform swipe
       top: 0,
       bottom: 0,
       child: AnimatedBuilder(
@@ -444,7 +438,6 @@ class _MessageContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isPinned = widget.isPinned;
-    // final pinnedBorderColor = isPinned ? Colors.amber[600]! : null;
 
     return Stack(
       children: [
@@ -471,9 +464,8 @@ class _MessageContainer extends StatelessWidget {
                 : null,
           ),
           child: Column(
-            crossAxisAlignment: widget.isSent
-                ? CrossAxisAlignment.end
-                : CrossAxisAlignment.start,
+            crossAxisAlignment:
+                CrossAxisAlignment.start, // Always start alignment
             children: [
               if (widget.isBeingRepliedTo)
                 _ReplyStatusIndicator(widget: widget),
@@ -507,8 +499,7 @@ class _ReplyStatusIndicator extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(
-        left: widget.isSent ? 0 : 50.w,
-        right: widget.isSent ? 50.w : 0,
+        left: 50.w, // Consistent margin
         bottom: 4.h,
       ),
       child: Row(
@@ -554,27 +545,25 @@ class _MainBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: widget.isSent
-          ? MainAxisAlignment.end
-          : MainAxisAlignment.start,
+      mainAxisAlignment:
+          MainAxisAlignment.start, // Always start - uniform alignment
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        if (!widget.isSent) ...[
-          Padding(
-            padding: EdgeInsets.only(top: 6.h),
-            child: _UserAvatar(messageData: widget.messageData),
-          ),
-          SizedBox(width: 8.w),
-        ],
+        // Always show avatar for both sent and received messages
+        Padding(
+          padding: EdgeInsets.only(top: 6.h),
+          child: _UserAvatar(messageData: widget.messageData),
+        ),
+        SizedBox(width: 8.w),
         Flexible(
           child: Bubble(
             padding: BubbleEdges.only(left: 3, right: 3),
             margin: BubbleEdges.only(top: 6),
-            alignment: widget.isSent ? Alignment.topRight : Alignment.topLeft,
+            alignment: Alignment.topLeft, // Always left aligned
             nipWidth: 18,
             nipHeight: 10,
             radius: Radius.circular(12.r),
-            nip: widget.isSent ? BubbleNip.rightTop : BubbleNip.leftTop,
+            nip: BubbleNip.leftTop, // Always left nip
             color: bubbleColor,
             child: SizedBox(
               child: Column(
@@ -583,12 +572,22 @@ class _MainBubble extends StatelessWidget {
                   if (widget.messageData?.type == 'CFVD') ...{
                     forwardIndicatorWhatsApp(),
                   },
-                  if (!widget.isSent &&
-                      widget.messageData?.sender?.name != null)
+                  // Always show sender name and message status
+                  if (widget.messageData?.sender?.name != null)
                     Padding(
                       padding: EdgeInsets.only(left: 3.w, bottom: 3.h),
-                      child: _SenderName(
-                        name: widget.messageData!.sender!.name!,
+                      child: Row(
+                        children: [
+                          _SenderName(name: widget.messageData!.sender!.name!),
+                          SizedBox(width: 8.w),
+                          // Show sent indicator for sent messages
+                          if (widget.isSent)
+                            Icon(
+                              Icons.done_all,
+                              size: 12,
+                              color: Colors.green[600],
+                            ),
+                        ],
                       ),
                     ),
                   if (widget.replyToMessage != null)
@@ -1701,9 +1700,7 @@ class _TimestampWithStatus extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: widget.isSent
-          ? MainAxisAlignment.end
-          : MainAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
         if (widget.isBeingRepliedTo) ...[
           Icon(Icons.reply, size: 10, color: Colors.blue.withOpacity(0.7)),
@@ -1719,291 +1716,6 @@ class _TimestampWithStatus extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-class MessageOptionsBottomSheet extends StatelessWidget {
-  final String message;
-  final bool isSent;
-  final bool isPinned;
-  final bool isBeingRepliedTo;
-  final VoidCallback? onReply;
-  final VoidCallback? onPin;
-  final VoidCallback? onCopy;
-  final VoidCallback? onDelete;
-  final VoidCallback? onDeleteEveryOne;
-  final VoidCallback? clearChat;
-  final VoidCallback? onforward;
-
-  const MessageOptionsBottomSheet({
-    super.key,
-    required this.message,
-    required this.isSent,
-    required this.isPinned,
-    this.isBeingRepliedTo = false,
-    this.onReply,
-    this.onPin,
-    this.onCopy,
-    this.onDelete,
-    this.onDeleteEveryOne,
-    this.clearChat,
-    this.onforward,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: BoxConstraints(
-        maxHeight: MediaQuery.of(context).size.height * 0.8,
-      ),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.vertical(top: Radius.circular(24.r)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 10,
-              offset: const Offset(0, -2),
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              width: 36.w,
-              height: 4.h,
-              margin: EdgeInsets.only(top: 12.h, bottom: 8.h),
-              decoration: BoxDecoration(
-                color: Colors.grey[400],
-                borderRadius: BorderRadius.circular(2.r),
-              ),
-            ),
-
-            if (message.isNotEmpty) ...[
-              Container(
-                width: double.infinity,
-                margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 8.h),
-                padding: EdgeInsets.all(12.w),
-                decoration: BoxDecoration(
-                  color: Colors.grey[50],
-                  borderRadius: BorderRadius.circular(12.r),
-                  border: Border.all(color: Colors.grey[200]!),
-                ),
-                child: Text(
-                  message.length > 50
-                      ? '${message.substring(0, 50)}...'
-                      : message,
-                  style: TextStyle(
-                    color: Colors.grey[600],
-                    fontSize: 14.sp,
-                    fontStyle: FontStyle.italic,
-                  ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-              Divider(
-                height: 1,
-                thickness: 1,
-                color: Colors.grey[200],
-                indent: 20.w,
-                endIndent: 20.w,
-              ),
-            ],
-
-            Flexible(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 0.h),
-                  child: Column(
-                    children: [
-                      _buildOption(
-                        icon: Icons.reply_rounded,
-                        title: 'Reply',
-                        onTap: onReply,
-                        isHighlighted: isBeingRepliedTo,
-                      ),
-                      _buildOption(
-                        icon: isPinned
-                            ? Icons.push_pin
-                            : Icons.push_pin_outlined,
-                        title: isPinned ? 'Unpin Message' : 'Pin Message',
-                        onTap: onPin,
-                      ),
-                      _buildOption(
-                        icon: Icons.content_copy_rounded,
-                        title: 'Copy Text',
-                        onTap: onCopy,
-                      ),
-                      _buildOption(
-                        icon: Icons.content_copy_rounded,
-                        title: 'Forward',
-                        onTap: onforward,
-                      ),
-
-                      Divider(
-                        height: 24.h,
-                        thickness: 1,
-                        color: Colors.grey[200],
-                        indent: 20.w,
-                        endIndent: 20.w,
-                      ),
-
-                      _buildOption(
-                        icon: Icons.delete_outline_rounded,
-                        title: 'Delete for Me',
-                        onTap: onDelete,
-                        isDestructive: true,
-                      ),
-                      if (isSent)
-                        _buildOption(
-                          icon: Icons.delete_sweep_outlined,
-                          title: 'Delete for Everyone',
-                          onTap: onDeleteEveryOne,
-                          isDestructive: true,
-                        ),
-                      _buildOption(
-                        icon: Icons.clear_all_rounded,
-                        title: 'Clear Chat',
-                        onTap: clearChat,
-                        isDestructive: true,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-
-            SizedBox(height: MediaQuery.of(context).padding.bottom + 20.h),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildOption({
-    required IconData icon,
-    required String title,
-    VoidCallback? onTap,
-    bool isDestructive = false,
-    bool isHighlighted = false,
-  }) {
-    final primaryColor = isDestructive
-        ? Colors.red[600]!
-        : isHighlighted
-        ? Colors.blue[600]!
-        : Colors.grey[800]!;
-
-    final backgroundColor = isHighlighted
-        ? Colors.blue[50]!
-        : Colors.transparent;
-
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 0.w, vertical: 0.h),
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        borderRadius: BorderRadius.circular(12.r),
-        border: isHighlighted
-            ? Border.all(color: Colors.blue[200]!, width: 1)
-            : null,
-      ),
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: onTap,
-          borderRadius: BorderRadius.circular(12.r),
-          splashColor: isDestructive
-              ? Colors.red.withOpacity(0.1)
-              : Colors.grey.withOpacity(0.1),
-          highlightColor: isDestructive
-              ? Colors.red.withOpacity(0.05)
-              : Colors.grey.withOpacity(0.05),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-            child: Row(
-              children: [
-                Container(
-                  width: 40.w,
-                  height: 40.h,
-                  decoration: BoxDecoration(
-                    color: primaryColor.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(10.r),
-                  ),
-                  child: Icon(icon, color: primaryColor, size: 20.sp),
-                ),
-                SizedBox(width: 16.w),
-                Expanded(
-                  child: Text(
-                    title,
-                    style: TextStyle(
-                      color: isDestructive ? Colors.red[700] : Colors.grey[800],
-                      fontSize: 14.sp,
-                      fontWeight: isHighlighted
-                          ? FontWeight.w600
-                          : FontWeight.w500,
-                      letterSpacing: -0.2,
-                    ),
-                  ),
-                ),
-                if (isHighlighted) ...[
-                  SizedBox(width: 8.w),
-                  Container(
-                    width: 24.w,
-                    height: 24.h,
-                    decoration: BoxDecoration(
-                      color: Colors.blue[100],
-                      borderRadius: BorderRadius.circular(6.r),
-                    ),
-                    child: Icon(
-                      Icons.keyboard_arrow_right_rounded,
-                      color: Colors.blue[700],
-                      size: 16.sp,
-                    ),
-                  ),
-                ],
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MediaContainer extends StatelessWidget {
-  final ChatMedias media;
-  final double? width;
-  final double? height;
-  final BorderRadius? borderRadius;
-
-  const MediaContainer({
-    super.key,
-    required this.media,
-    this.width,
-    this.height,
-    this.borderRadius,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: height,
-      decoration: BoxDecoration(
-        borderRadius: borderRadius ?? BorderRadius.circular(8.r),
-        border: Border.all(color: Colors.grey[300]!),
-      ),
-      child: ClipRRect(
-        borderRadius: borderRadius ?? BorderRadius.circular(8.r),
-        child: OptimizedMediaPreview(
-          media: media,
-          maxWidth: width,
-          maxHeight: height,
-        ),
-      ),
     );
   }
 }
--- lib/shared/widgets/media/media_cache.dart
+++ lib/shared/widgets/media/media_cache.dart
@@ -1,5 +1,10 @@
 import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:soxo_chat/feature/chat/domain/models/chat_entry/chat_entry_response.dart';
+import 'package:soxo_chat/feature/chat/screen/widgets/chat_bubble_widget.dart';
+
 class MediaCache {
   static final Map<String, Uint8List> _imageCache = {};
   static final Map<String, String> _filePathCache = {};
@@ -57,5 +62,290 @@ class MediaCache {
     _filePathCache.remove(key);
     _loadingItems.remove(key);
     _failedItems.remove(key);
+  }
+}
+
+class MessageOptionsBottomSheet extends StatelessWidget {
+  final String message;
+  final bool isSent;
+  final bool isPinned;
+  final bool isBeingRepliedTo;
+  final VoidCallback? onReply;
+  final VoidCallback? onPin;
+  final VoidCallback? onCopy;
+  final VoidCallback? onDelete;
+  final VoidCallback? onDeleteEveryOne;
+  final VoidCallback? clearChat;
+  final VoidCallback? onforward;
+
+  const MessageOptionsBottomSheet({
+    super.key,
+    required this.message,
+    required this.isSent,
+    required this.isPinned,
+    this.isBeingRepliedTo = false,
+    this.onReply,
+    this.onPin,
+    this.onCopy,
+    this.onDelete,
+    this.onDeleteEveryOne,
+    this.clearChat,
+    this.onforward,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: MediaQuery.of(context).size.height * 0.8,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(24.r)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10,
+              offset: const Offset(0, -2),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 36.w,
+              height: 4.h,
+              margin: EdgeInsets.only(top: 12.h, bottom: 8.h),
+              decoration: BoxDecoration(
+                color: Colors.grey[400],
+                borderRadius: BorderRadius.circular(2.r),
+              ),
+            ),
+
+            if (message.isNotEmpty) ...[
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 8.h),
+                padding: EdgeInsets.all(12.w),
+                decoration: BoxDecoration(
+                  color: Colors.grey[50],
+                  borderRadius: BorderRadius.circular(12.r),
+                  border: Border.all(color: Colors.grey[200]!),
+                ),
+                child: Text(
+                  message.length > 50
+                      ? '${message.substring(0, 50)}...'
+                      : message,
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 14.sp,
+                    fontStyle: FontStyle.italic,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Divider(
+                height: 1,
+                thickness: 1,
+                color: Colors.grey[200],
+                indent: 20.w,
+                endIndent: 20.w,
+              ),
+            ],
+
+            Flexible(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 0.h),
+                  child: Column(
+                    children: [
+                      _buildOption(
+                        icon: Icons.reply_rounded,
+                        title: 'Reply',
+                        onTap: onReply,
+                        isHighlighted: isBeingRepliedTo,
+                      ),
+                      _buildOption(
+                        icon: isPinned
+                            ? Icons.push_pin
+                            : Icons.push_pin_outlined,
+                        title: isPinned ? 'Unpin Message' : 'Pin Message',
+                        onTap: onPin,
+                      ),
+                      _buildOption(
+                        icon: Icons.content_copy_rounded,
+                        title: 'Copy Text',
+                        onTap: onCopy,
+                      ),
+                      _buildOption(
+                        icon: Icons.forward,
+                        title: 'Forward',
+                        onTap: onforward,
+                      ),
+
+                      Divider(
+                        height: 24.h,
+                        thickness: 1,
+                        color: Colors.grey[200],
+                        indent: 20.w,
+                        endIndent: 20.w,
+                      ),
+
+                      _buildOption(
+                        icon: Icons.delete_outline_rounded,
+                        title: 'Delete for Me',
+                        onTap: onDelete,
+                        isDestructive: true,
+                      ),
+                      if (isSent)
+                        _buildOption(
+                          icon: Icons.delete_sweep_outlined,
+                          title: 'Delete for Everyone',
+                          onTap: onDeleteEveryOne,
+                          isDestructive: true,
+                        ),
+                      _buildOption(
+                        icon: Icons.clear_all_rounded,
+                        title: 'Clear Chat',
+                        onTap: clearChat,
+                        isDestructive: true,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: MediaQuery.of(context).padding.bottom + 20.h),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOption({
+    required IconData icon,
+    required String title,
+    VoidCallback? onTap,
+    bool isDestructive = false,
+    bool isHighlighted = false,
+  }) {
+    final primaryColor = isDestructive
+        ? Colors.red[600]!
+        : isHighlighted
+        ? Colors.blue[600]!
+        : Colors.grey[800]!;
+
+    final backgroundColor = isHighlighted
+        ? Colors.blue[50]!
+        : Colors.transparent;
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 0.w, vertical: 0.h),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(12.r),
+        border: isHighlighted
+            ? Border.all(color: Colors.blue[200]!, width: 1)
+            : null,
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(12.r),
+          splashColor: isDestructive
+              ? Colors.red.withOpacity(0.1)
+              : Colors.grey.withOpacity(0.1),
+          highlightColor: isDestructive
+              ? Colors.red.withOpacity(0.05)
+              : Colors.grey.withOpacity(0.05),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+            child: Row(
+              children: [
+                Container(
+                  width: 40.w,
+                  height: 40.h,
+                  decoration: BoxDecoration(
+                    color: primaryColor.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(10.r),
+                  ),
+                  child: Icon(icon, color: primaryColor, size: 20.sp),
+                ),
+                SizedBox(width: 16.w),
+                Expanded(
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      color: isDestructive ? Colors.red[700] : Colors.grey[800],
+                      fontSize: 14.sp,
+                      fontWeight: isHighlighted
+                          ? FontWeight.w600
+                          : FontWeight.w500,
+                      letterSpacing: -0.2,
+                    ),
+                  ),
+                ),
+                if (isHighlighted) ...[
+                  SizedBox(width: 8.w),
+                  Container(
+                    width: 24.w,
+                    height: 24.h,
+                    decoration: BoxDecoration(
+                      color: Colors.blue[100],
+                      borderRadius: BorderRadius.circular(6.r),
+                    ),
+                    child: Icon(
+                      Icons.keyboard_arrow_right_rounded,
+                      color: Colors.blue[700],
+                      size: 16.sp,
+                    ),
+                  ),
+                ],
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MediaContainer extends StatelessWidget {
+  final ChatMedias media;
+  final double? width;
+  final double? height;
+  final BorderRadius? borderRadius;
+
+  const MediaContainer({
+    super.key,
+    required this.media,
+    this.width,
+    this.height,
+    this.borderRadius,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: borderRadius ?? BorderRadius.circular(8.r),
+        border: Border.all(color: Colors.grey[300]!),
+      ),
+      child: ClipRRect(
+        borderRadius: borderRadius ?? BorderRadius.circular(8.r),
+        child: OptimizedMediaPreview(
+          media: media,
+          maxWidth: width,
+          maxHeight: height,
+        ),
+      ),
+    );
   }
 }
